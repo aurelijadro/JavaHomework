d34bb2390a318ef60262215d8dece3e42b727779
Aurelija <a.drobelyte@gmail.com>
2019-09-24 15:34:52 +0300

    PD 6 part 1 and 2

--- pd6-javasintakse/src/lt/vtmc/pd6/JavaSintakse.java
+++ pd6-javasintakse/src/lt/vtmc/pd6/JavaSintakse.java
@@ -1,10 +1,27 @@
 package lt.vtmc.pd6;
 
-public class JavaSintakse {
-
+class JavaSintakse {
+	 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//Primitive JAVA types:
+		byte num = 1; // 1 byte
+		short num2 = 2; // 2 bytes
+		int num3 = 3; // 4 bytes
+		long num4 = 1234567890123456789L; // 8 bytes
+		float num5 = 10.123F; // 4 bytes
+		double num6 = 123456789.0123; // 8 bytes
+		boolean correct=true; // 1 byte
+		char randomletter='a'; // 2 bytes
+		// Print all different values
+		System.out.println(num);
+		System.out.println(num2);
+		System.out.println(num3);
+		System.out.println(num4);
+		System.out.println(num5);
+		System.out.println(num6);
+		System.out.println(correct);
+		System.out.println(randomletter);
+		
 	}
 
 }
