78dfbb17dc6141d87838e3a8b2bc48f5d16e2bd5
aurelija <a.drobelyte@gmail.com>
2019-10-13 00:37:42 +0300

    rewritten calculator

--- src/new_calculator/Count.java
+++ src/new_calculator/Count.java
@@ -3,6 +3,8 @@ package new_calculator;
 public class Count {
 	public static void main(String[] args) {
 		System.out.println(Calculator.add(14.165894, 13.99999));
+		System.out.println(Calculator.calculateSquareRoot(9));
+		System.out.println(Calculator.divide(7, 2));
 		System.out.println(Calculator.calculatePercent(100, 20));
 	}
 }
